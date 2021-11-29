e7aa15d30707528545265cbe5f335181cf826a08
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-29 22:05:04 +0530

    [vishal] Avarege Rating  DataTable and Record in recview managment

---
+++ Uc10_ind AverageRatingProductReview/ProductReviews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc10_ind_AverageRatingProductReview
+{
+    class ProductReviews
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public float Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
